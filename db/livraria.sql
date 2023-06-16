6c93eca926703f53e39c177bce697405d709fd7d
EduardoJesusx <srduduca@gmail.com>
2023-06-16 17:51:46 -0300

    update db

--- db/livraria.sql
+++ db/livraria.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15-Jun-2023 às 21:15
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 7.4.29
+-- Tempo de geração: 16-Jun-2023 às 22:50
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,30 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `adm`
---
-
-CREATE TABLE `adm` (
-  `email` varchar(200) NOT NULL,
-  `senha` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `adm`
---
-
-INSERT INTO `adm` (`email`, `senha`) VALUES
-('adm@adm.com', 'admadm');
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `capas`
 --
 
 CREATE TABLE `capas` (
   `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -60,27 +42,28 @@ CREATE TABLE `cliente` (
   `nome_cliente` varchar(200) NOT NULL,
   `sobrenome` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `senha` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `senha` varchar(200) NOT NULL,
+  `admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Extraindo dados da tabela `cliente`
 --
 
-INSERT INTO `cliente` (`id_cliente`, `nome_cliente`, `sobrenome`, `email`, `senha`) VALUES
-(1, 'Mario', 'Henrique', 'mario@hrllo.com', '5d41402abc4b2a76b9719d911017c592'),
-(2, 'as', 'asa', 'asasa@sasa.com', 'a2e63ee01401aaeca78be023dfbb8c59'),
-(3, 'lo', 'lo', 'lo@lo', '9cdfb439c7876e703e307864c9167a15'),
-(4, 'Mario', 'Pelissari', 'mario@henrique.com.br', 'b21afc54fb48d153c19101658f4a2a48'),
-(5, 'Rosane ', 'Pelissari', 'rose@gmail.com', '54b1d109dc7156ef46816a9527a861bc'),
-(6, 'Lucas', 'Barbosa', 'Figma@gmail.com', 'a2e63ee01401aaeca78be023dfbb8c59'),
-(7, 'Paolo', 'asa', 'Pal@gmail.com', '12'),
-(8, 'Luana', 'Risk', 'lu@gmail.com', '654e4dc5b90b7478671fe6448cab3f32'),
-(9, 'Helo', 'pop', 'hello@gmail.com', 'a2e63ee01401aaeca78be023dfbb8c59'),
-(10, 'Yuri', 'lico', 'yuri@gmail.com', '9491876179d7a80bb5c86f15dbe31422'),
-(11, 'Maro', 'poli', 'maro@gmail.com', 'senha'),
-(12, 'lucas', 'Querry', 'luca@gmail.com', '68053af2923e00204c3ca7c6a3150cf7'),
-(13, 'dudu', 'camarco', '123@123', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `cliente` (`id_cliente`, `nome_cliente`, `sobrenome`, `email`, `senha`, `admin`) VALUES
+(1, 'Mario', 'Henrique', 'mario@hrllo.com', '5d41402abc4b2a76b9719d911017c592', 0),
+(2, 'as', 'asa', 'asasa@sasa.com', 'a2e63ee01401aaeca78be023dfbb8c59', 0),
+(3, 'lo', 'lo', 'lo@lo', '9cdfb439c7876e703e307864c9167a15', 0),
+(4, 'Mario', 'Pelissari', 'mario@henrique.com.br', 'b21afc54fb48d153c19101658f4a2a48', 0),
+(5, 'Rosane ', 'Pelissari', 'rose@gmail.com', '54b1d109dc7156ef46816a9527a861bc', 0),
+(6, 'Lucas', 'Barbosa', 'Figma@gmail.com', 'a2e63ee01401aaeca78be023dfbb8c59', 0),
+(7, 'Paolo', 'asa', 'Pal@gmail.com', '12', 0),
+(8, 'Luana', 'Risk', 'lu@gmail.com', '654e4dc5b90b7478671fe6448cab3f32', 0),
+(9, 'Helo', 'pop', 'hello@gmail.com', 'a2e63ee01401aaeca78be023dfbb8c59', 0),
+(10, 'Yuri', 'lico', 'yuri@gmail.com', '9491876179d7a80bb5c86f15dbe31422', 0),
+(11, 'Maro', 'poli', 'maro@gmail.com', 'senha', 0),
+(12, 'lucas', 'Querry', 'luca@gmail.com', '68053af2923e00204c3ca7c6a3150cf7', 0),
+(13, 'dudu', 'camarco', '123@123', '202cb962ac59075b964b07152d234b70', 1);
 
 -- --------------------------------------------------------
 
@@ -95,7 +78,7 @@ CREATE TABLE `livros` (
   `editora` varchar(200) NOT NULL,
   `ano_publi` varchar(4) NOT NULL,
   `imagem` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Extraindo dados da tabela `livros`
@@ -142,7 +125,7 @@ ALTER TABLE `capas`
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `livros`
